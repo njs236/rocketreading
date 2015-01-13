dd0c39107423dc47ebe9d5bffd08c274031c76b1
njs236 <njs236@student.cpit.ac.nz>
2015-01-14 11:28:16 +1300

    Create testingrecognition.spec.js
    This is jasmine for the testingrecognition feature

---
+++ spec/testingrecognition.spec.js
@@ -0,0 +1,22 @@
+describe ( "to test my comprehension", function () {
+  it("is below 2 seconds", function() {
+    expect(selectWord("I").toequal("I")
+  }
+}
+it("is between 2 and 4 seconds", function() {
+    expect(selectWord("I").toEqual("I")
+  }
+}
+it("is between 4 and 8 seconds", function() {
+    expect(selectWord("I").toEqual("I")
+  }
+}
+it("is incorrect word", function() {
+    expect(selectWord("I").not.toEqual("am")
+  }
+}
+it("is over 8s", function() {
+    expect(selectWord("I").toEqual("I")
+  }
+}
+  );

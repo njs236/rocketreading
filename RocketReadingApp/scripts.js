79ef41c5d59ac8d7c9fd614c0443585e7fd2e00f
Michael Holmes <mih279@student.cpit.ac.nz>
2015-01-27 20:14:26 +1300

    Removing accidental changes to files
    I forgot to change to my branch before coding

--- RocketReadingApp/scripts.js
+++ RocketReadingApp/scripts.js
@@ -5,34 +5,3 @@ var HTMLTABLE; // mapped to table that displays words
 function showPage1() {
 	console.log("hi");
 };
-
-var createHighTable = function (tableDim, type) {
-	"use strict";
-	var newRow,
-		newCell,
-		wordCount;
-
-	HTMLTABLE.innerHTML = ""; // clear table each time its run
-	newRow = HTMLTABLE.insertRow(-1);
-
-	for ( wordCount = 0; wordCount < tableDim[0]; wordCount += 1) {
-		if ( HTMLTABLE.lastChild.lastChild.cells.length === tableDim[1] ) {
-			newRow = HTMLTABLE.insertRow(-1);
-		};
-		newCell = newRow.insertCell(-1);
-		/*newCell.innerHTML = inputArray[wordCount];*/
-		newCell.id = "cell" + type + (wordCount + 1);
-		newCell.className = "HighCell";
-	};
-};
-
-var initialise = function () {
-	"use strict";
-    var highCells = [24, 4],
-        bonusCells = [6, 6];
-    HTMLTABLE = document.getElementById("highLevelGamesTable");
-    // Create the high score table for the level games
-    createHighTable(highCells, "Level");
-    // Create the high score table for the bonus games
-    createHighTable(bounsCells, "Bonus");
-};

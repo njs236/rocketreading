06bbef4186ed6128ad37c143a278d39ee1f44b28
Sean <seanfuss+github@gmail.com>
2015-01-29 12:25:21 +1300

    Lists 1 & 2 hardcoded
    function now hard codes lists 1 and 2

--- RocketReadingApp/scripts.js
+++ RocketReadingApp/scripts.js
@@ -1,3 +1,23 @@
+var gameLists = [];
+
 function showPage1() {
 	console.log("hi");
+}
+
+// load lists
+
+function generateLists() {
+	"use strict"
+	var wordList = [],
+		count;
+	
+	gameLists = [];
+	// List 1
+	wordList = ["I","am","the","go","going","to","shop","car","at","in","is","Mum","here","and","see","Dad","a","cat","can","said","run","running","jump","jumping","school"];
+	
+	gameLists.push(wordList);
+
+	wordList = ["we","are","look","dog","come","bus","play","this","there","went","with","asked","bat","hit","ball","kicked","red","boy","girls","big","like","computer","TV","watch","home"];
+	
+	gameLists.push(wordList);
 }

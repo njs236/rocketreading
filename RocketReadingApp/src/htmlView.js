d34ad282096a4d896125013ce4d6f32d582dcc70
Sean <themorselisstring@users.noreply.github.com>
2015-02-06 16:12:36 +1300

    hideAll() -> hideAllPages ()
    Renames function, and it takes an input. Code commented

--- RocketReadingApp/src/htmlView.js
+++ RocketReadingApp/src/htmlView.js
@@ -8,12 +8,13 @@ var HTMLView = function (newName, theViewModel) {
 	this.myViewModel = theViewModel || {};
 };
 
-HTMLView.prototype.hideAll = function () {
+HTMLView.prototype.hideAllPages = function (inputDivClass) {
+	// This function takes a class name of the divs used to created 'pages'
+	// and hides all elements with that class name
 	"use strict";
 	var screens = [],
 		count;
-	
-	screens = document.getElementsByClassName("navScreen");
+	screens = document.getElementsByClassName(inputDivClass);
 	for ( count = 0; count < screens.length; count = count + 1 ) {
 		console.log("hideAll() : Hiding " + screens[count].id);
 		screens[count].hidden = true;

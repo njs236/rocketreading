0f374c799045ae8aabd74a9bd887c18403b69840
Sean <themorselisstring@users.noreply.github.com>
2015-02-06 17:16:06 +1300

    Refactor HTML module

--- RocketReadingApp/src/htmlView.js
+++ RocketReadingApp/src/htmlView.js
@@ -3,20 +3,53 @@
 //
 // Contains the code for hiding the divs
 
-var HTMLView = function (newName, theViewModel) {
-	this.name = newName || "unnamed";
-	this.myViewModel = theViewModel || {};
-};
-
-HTMLView.prototype.hideAllPages = function (inputDivClass) {
-	// This function takes a class name of the divs used to created 'pages'
-	// and hides all elements with that class name
-	"use strict";
-	var screens = [],
-		count;
-	screens = document.getElementsByClassName(inputDivClass);
-	for ( count = 0; count < screens.length; count = count + 1 ) {
-		console.log("hideAll() : Hiding " + screens[count].id);
-		screens[count].hidden = true;
+var viewHTMLModule = {
+	hideAllPages: function (inputClassName) {
+		"use strict";
+		var screens = [],
+			count;
+		screens = document.getElementsByClassName(inputClassName);
+		for ( count = 0; count < screens.length; count = count + 1 ) {
+			console.log("hideAll() : Hiding " + screens[count].id);
+			screens[count].hidden = true;
+		};
 	};
-};
+	
+	showLoginScreen: function () {
+		"use strict";
+		hideAllPages();
+		document.getElementById("loginScreen").hidden = false;
+	};
+	
+	showHomeScreen: function () {
+		"use strict";
+		hideAllPages();
+		document.getElementById("homeScreen").hidden = false;
+	};
+	
+	showLevelSelectScreen: function () {
+		"use strict";
+		hideAllPages();
+		document.getElementById("levelSelectScreen").hidden = false;
+	};
+	
+	showGameSelectScreen: function () {
+		"use strict";
+		hideAllPages();
+		document.getElementById("gameSelectScreen").hidden = false;
+	};
+	
+	showGameScreen: function () {
+		"use strict";
+		hideAllPages();
+		document.getElementById("gamesScreen").hidden = false;
+	};
+	
+	showHighScoresScreen: function () {
+		"use strict";
+		hideAllPages();
+		document.getElementById("highScoresScreen").hidden = false;
+	};
+	
+	
+}

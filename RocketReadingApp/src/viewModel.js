2a5c7574a8fcbbd362416547ca5d20b1bd5abfd1
michaeljh17 <mih279@student.cpit.ac.nz>
2015-02-06 15:14:53 +1300

    Updating the viewModel object
    It now includes a consoleView: data and information can be written in
    the console (for testing purposes).

--- RocketReadingApp/src/viewModel.js
+++ RocketReadingApp/src/viewModel.js
@@ -1,8 +1,9 @@
 // Creating a viewModel object for the system
 
 var myViewModelRR = {
-    displayMethods: {
-        inputGamesRequiredLevel: function (levelType) {
+    displayHTMLMethods: {
+        // I am thinking of giving all methods which will involve the display of HTML elements the suffix (or prefix) HTML
+        gamesRequiredLevelHTML: function (levelType) {
             "use strict";
             var result;
             // There are two ways of displaying the selection of games based on the type of level which the user is looking at
@@ -15,6 +16,21 @@ var myViewModelRR = {
             }
             // A function in the viewHTML object is then called, which determines how to display the selection of games in the UI
             displayGamesSelection(result);
+        }
+    }
+    
+    displayConsoleMethods: {
+        // I am thinking of giving all methods which will involve displaying data in the console the suffix (or prefix) Console
+        gamesRequiredLevelConsole: function (levelType) {
+        "use strict";
+        var result;
+        if (levelType === "WordList") {
+            console.log("Type of games to be displayed: WordListGames."
+            // Is the following too much information for the viewModel to know about or determine. However, the number of games to be displayed should or could be passed to it from the mainController. The number of games would then be used by the viewModel to determine the functions and arguments which will then be called.
+            console.log("Number of games to be displayed: 4");
+        } else if (levelType === "BonusList") {
+            console.log("Type of games to be displayed: BonusGames.");
+            console.log("Titles of bonus games to be displayed: Alphabet Sounds", "Constant Blends 1","Constant Blends 2","Short Vowel Sounds","Long Vowel Sounds","Middle Sounds");
         }
     }
 };

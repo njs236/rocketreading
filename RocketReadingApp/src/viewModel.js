4d48a70a104bb12dd63a179f7fa8ae11e08e9b01
michaeljh17 <mih279@student.cpit.ac.nz>
2015-02-06 16:48:48 +1300

    Fixing syntax errors
    Small changes to the viewModel.js file

--- RocketReadingApp/src/viewModel.js
+++ RocketReadingApp/src/viewModel.js
@@ -17,20 +17,23 @@ var myViewModelRR = {
             // A function in the viewHTML object is then called, which determines how to display the selection of games in the UI
             displayGamesSelection(result);
         }
-    }
+    },
+    
+ 
     
     displayConsoleMethods: {
         // I am thinking of giving all methods which will involve displaying data in the console the suffix (or prefix) Console
         gamesRequiredLevelConsole: function (levelType) {
-        "use strict";
-        var result;
-        if (levelType === "WordList") {
-            console.log("Type of games to be displayed: WordListGames."
-            // Is the following too much information for the viewModel to know about or determine. However, the number of games to be displayed should or could be passed to it from the mainController. The number of games would then be used by the viewModel to determine the functions and arguments which will then be called.
-            console.log("Number of games to be displayed: 4");
-        } else if (levelType === "BonusList") {
-            console.log("Type of games to be displayed: BonusGames.");
-            console.log("Titles of bonus games to be displayed: Alphabet Sounds", "Constant Blends 1","Constant Blends 2","Short Vowel Sounds","Long Vowel Sounds","Middle Sounds");
+            "use strict";
+            var result;
+            if (levelType === "WordList") {
+                console.log("Type of games to be displayed: WordListGames."
+                // Is the following too much information for the viewModel to know about or determine. However, the number of games to be displayed should or could be passed to it from the mainController. The number of games would then be used by the viewModel to determine the functions and arguments which will then be called.
+                console.log("Number of games to be displayed: 4");
+            } else if (levelType === "BonusList") {
+                console.log("Type of games to be displayed: BonusGames.");
+                console.log("Titles of bonus games to be displayed: Alphabet Sounds", "Constant Blends 1","Constant Blends 2","Short Vowel Sounds","Long Vowel Sounds","Middle Sounds");
+            }
         }
     }
 };

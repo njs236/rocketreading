a1eafae7731c211b5e25bdbb32143cadb2d11bbb
Sean <seanfuss+github@gmail.com>
2015-02-09 16:18:42 +1300

    File Cleanup
    DIE jquery! die bootstrap

--- RocketReadingApp/src/modelRR.js
+++
@@ -1,8 +0,0 @@
-// The system creates a model object for the Rocket Reading system
-// Actually, the rocketReadingController object can act as the model object for the system.
-
-var modelRR = {
-    this.findNumGamesOfLevel: function () {
-        "use strict";    
-    }
-};

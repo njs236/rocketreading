5620a90603434a4eaec25d01948e5247ecaf23dc
Michael Holmes <mih279@student.cpit.ac.nz>
2015-02-10 10:50:24 +1300

    Changes to scripts.js
    Added the bonus game sounds-lists to the scripts file so that they can
    be loaded into the system.

--- RocketReadingApp/scripts.js
+++ RocketReadingApp/scripts.js
@@ -51,7 +51,95 @@ var loadGameData = function () {
         wordList21 = ["join","soil","space","chimney","cute","comb","group","although","belong","choose","country","toe","thirsty","smell","shell","rules","ruler","outside","believe","newspaper","nearly","shove","square","queen","quick"],
         wordList22 = ["petrol","notice","neighbour","wool","voyage","fruit","fried","feel","enough","dirt","care","careful","large","meeting","lose","nuisance","piece","parcel","hole","whole","troll","life","leaf","yacht","weigh"],
         wordList23 = ["bridge","cupboard","both","ceiling","fierce","follow","loud","stroll","stair","tui","feast","mould","mistake","kneel","learn","minute","luck","owl","overeat","point","pie","question","rescue","return","poem"],
-        wordList24 = ["preview","protein","roast","spill","shoulder","skull","scared","towel","salt","soup","hundred","seize","sulk","stung","cellphone","cracked","float","fainted","fetch","internet","browse","world","wrong","galaxy","wrapped"];
+        wordList24 = ["preview","protein","roast","spill","shoulder","skull","scared","towel","salt","soup","hundred","seize","sulk","stung","cellphone","cracked","float","fainted","fetch","internet","browse","world","wrong","galaxy","wrapped"],
+        
+        alphabetSoundsList = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+        constantBlends1List = ["bl","br","ch","cl","cr","dr","dw","fl","fr","gl","gr","kn","ph","pl","pr","qu","sc","scr"],
+        // I'm not sure at which point of the blends sound list it should be divided into two
+        constantBlends2List= ["sh","shr","sk","sl","sm","sn","sp","spl","spr","squ","st","str","sw","th","thr","tr","tw","wh","wr"],
+        
+        shortVowelsList = [],
+        shortVowelsList_a = ["an","man","bat","tap","and","sad","am","bag","ran","had","that","drag","slap","slam","brat"],
+        shortVowelsList_e = ["get","net","beg","red","pen","yes","fed","bed","ten","egg","then","went","bled","Greg","step"],
+        shortVowelsList_i = ["pin","hit","rip","rib","nip","his","six","did","big","if","chin","think","spit","this","hit"],
+        shortVowelsList_o = ["got","job","hog","box","mop","Tom","sob","from","pot","dog","shop","stop","blog","shot","blob"],
+        shortVowelsList_u = ["fun","cup","bus","hug","tub","mud","us","up","mum","but","shut","slug","club","drum","under"],
+        
+        longVowelsList = [],
+        longVowelsList_a = ["made","bake","came","gave","Jane","ate","base","space","safe","grape","play","say","bay","ray","away"],
+        longVowelsList_e = ["see","we","she","bee","sea","meat","keep","please","teacher","read","been","tree","each","three","people"],
+        longVowelsList_i = ["I","ride","bike","time","ripe","rise","bite","wipe","fire","mice","kind","fine","white","while","climb"],
+        longVowelsList_o = ["go","home","nose","over","hope","vote","rode","don't","open","joke","bone","hole","note","old","know"],
+        longVowelsList_u = ["use","usually","rule","rude","pupil","truth","tune","cute","music","refuse","uniform","universe","future","amused","dune"],
+        longVowelsList_y = ["by","why","my","cry","fly","shy","happy","baby","very","many","ready","only","pretty","key","tiny"],
+        
+        middleSoundsList = [],
+        middleSoundsList_oo1 = ["look","cook","good","shook","stood","wool"],
+        middleSoundsList_ay = ["day","play","may","say","away","pray"],
+        middleSoundsList_ee = ["see","bee","feed","meet","teeth","sleep"],
+        middleSoundsList_all = ["ball","call","fall","small","wall", "shall"],
+        middleSoundsList_ar = ["car","star","farm","shark","party","garden"],
+        middleSoundsList_er = ["her","were","mother","term","person","fern"],
+        middleSoundsList_ow_1 = ["how","now","town","down","brown","flower"],
+        middleSoundsList_or = ["for","horse","sport","more","storm","shore"],
+        middleSoundsList_ai_1 = ["rain","tail","train","painting","wait","faint"],
+        middleSoundsList_oy = ["boy","toy","joy","enjoy","annoy","voyage"],
+        middleSoundsList_oo_2 = ["moon","food","boot","choose","soon","blood"],
+        middleSoundsList_oa = ["boat","float","road","roast","soap","coat"],
+        middleSoundsList_ew = ["new","few","chew","blew","flew","grew"],
+        middleSoundsList_ow_2 = ["slow","blow","grow","know","throw","follow"],
+        middleSoundsList_aw = ["saw","awful","draw","raw","awesome","lawn"],
+        middleSoundsList_ill_ell = ["hill","will","fell","spell","smell","spill"],
+        middleSoundsList_ea1 = ["meat","seat","beach","team","feast","cheat"],
+        middleSoundsList_ou_1 = ["out","house","found","shout","ground","ouch"],
+        middleSoundsList_ou_2 = ["your","four","brought","thought","fourteen","fought"],
+        middleSoundsList_ou_3 = ["enough","trouble","cousin","touch","young","courage"],
+        middleSoundsList_ou_4 = ["you","through","group","soup","cough","trough"],
+        middleSoundsList_ou_5 = ["would","could","should","although","shoulder","mould"],
+        middleSoundsList_ir = ["girl","bird","birthday","first","thirty","dirt"],
+        middleSoundsList_oll_ull = ["doll","roll","skull","seagull","troll","dull"],
+        middleSoundsList_one = ["one","done","love","oven","cover","none"],
+        middleSoundsList_ue = ["blue","glue","argue","true","rescue","continue"],
+        middleSoundsList_ie_1 = ["pie","die","lie","tried","cried","fried"],
+        middleSoundsList_eel = ["eel","feel","heel","wheel","steel","kneel"],
+        middleSoundsList_ea_2 = ["head","bear","dead","meal","ready","weather"],
+        middleSoundsList_ur = ["burn","hurt","turning","nurse","surfer","church"],
+        middleSoundsList_oi = ["oil","join","toilet","soil","noise","point"],
+        middleSoundsList_ai_2 = ["said","hair","chair","stair","air","unfair"],
+        middleSoundsList_au = ["caught","naughty","saucer","daughter","because","laugh"],
+        middleSoundsList_ie_2 = ["chief","thief","piece","believe","field","shield"],
+        middleSoundsList_ei_1 = ["receive","seize","protein","deceive","ceiling","their"],
+        middleSoundsList_ei_2 = ["eight","weigh","reindeer","neighbour","weight","vein"],
+        middleSoundsList_ui = ["fruit","nuisance","build","tui","quiet","guide"],
+        middleSoundsList_ea_3 = ["learn","heard","early","earth","earn","pearl"],
+        middleSoundsList_are = ["care","dare","stare","careful","scared","bare"],
+        middleSoundsList_oe = ["toe","poem","poetry","ice-floe","shoe","does"],
+        middleSoundsList_ow = ["owl","growl","towel","fowl","howl","foul"],
+        middleSoundsList_igh = ["high","night","bright","sighed","knight","Friday"],
+        middleSoundsList_lk = ["milk","talk","walk","sulk","folk","elk"],
+        middleSoundsList_st = ["fast","best","most","mustn't","list","ghostly"],
+        middleSoundsList_ck = ["blackness","lucky","check","bricks","blocked","cracked"],
+        middleSoundsList_ch = ["watch","catch","fetch","which","such","kitchen"],
+        middleSoundsList_ng = ["song","sing","ring","bang","longest","stung"];
+        
+    // Add all of the short vowel lists to one array
+    shortVowelsList.push(shortVowelsList_a);    
+    shortVowelsList.push(shortVowelsList_e);
+    shortVowelsList.push(shortVowelsList_i);
+    shortVowelsList.push(shortVowelsList_o);
+    shortVowelsList.push(shortVowelsList_u);
+    
+    // Add all of the long vowel lists to one array
+    longVowelsList.push(longVowelsList_a);
+    longVowelsList.push(longVowelsList_e);
+    longVowelsList.push(longVowelsList_i);
+    longVowelsList.push(longVowelsList_o);
+    longVowelsList.push(longVowelsList_u);
+    longVowelsList.push(longVowelsList_y);
+    
+    // Add all of the middle sounds lists to one array
+    middleSoundsList.push(middleSoundsList_oo1);
+    /* and so on ... */
     
     // The system will delete any old instances of objects in the system
     // rocketReadingModel.deleteAll(); This isn't necessary
@@ -72,21 +160,15 @@ var loadGameData = function () {
     rocketReadingModel.addLevel("Lollipop World", 4, [], 0);
     rocketReadingModel.addLevel("Pirate World", 5, [], 0);
     rocketReadingModel.addLevel("Car World", 6, [], 0);
-    /*
-
-    theLevel = rocketReadingModel.findLevel("Bonus Games Level");
-    theLevel.addGame("Alphabet Sounds", alphabetSoundsList);
-    theLevel.addGame("Constant Blends 1", constantBlends1List);
-    theLevel.addGame("Constant Blends 2", constantBlends2List);
-    theLevel.addGame("Short Vowel Sounds", shortVowelsList);
-    theLevel.addGame("Long Vowels Sounds", longVowelsList);
-    theLevel.addGame("Middle Sounds", middleSoundsList);*/
+    
     theLevel = rocketReadingModel.findLevelByNumber(0);
-	rocketReadingModel.addAvatar("star", theLevel);
-    theLevel.addGame("Game 1", wordList1, theLevel.name);
-    theLevel.addGame("Game 2", wordList2, theLevel.name);
-    theLevel.addGame("Game 3", wordList3, theLevel.name);
-    theLevel.addGame("Game 4", wordList4, theLevel.name);
+    rocketReadingModel.addAvatar("star", theLevel);
+    theLevel.addGame("Alphabet Sounds", alphabetSoundsList, theLevel.name);
+    theLevel.addGame("Constant Blends 1", constantBlends1List, theLevel.name);
+    theLevel.addGame("Constant Blends 2", constantBlends2List, theLevel.name);
+    theLevel.addGame("Short Vowel Sounds", shortVowelsList, theLevel.name);
+    theLevel.addGame("Long Vowels Sounds", longVowelsList, theLevel.name);
+    theLevel.addGame("Middle Sounds", middleSoundsList, theLevel.name);
 	
     theLevel = rocketReadingModel.findLevelByNumber(1);
 	rocketReadingModel.addAvatar("icecream", theLevel);

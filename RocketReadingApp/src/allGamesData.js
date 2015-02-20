2559a7e66edd1c6ed4bd6165097b378fd157c661
michaeljh17 <mih279@student.cpit.ac.nz>
2015-02-21 07:10:18 +1300

    Fixing a login bug
    When a user logged in to the game, the allGamesData object was not being
    properly created by the rocketReadingModel's setPlayer() function.

--- RocketReadingApp/src/allGamesData.js
+++ RocketReadingApp/src/allGamesData.js
@@ -37,98 +37,96 @@ var AllGamesData = function (newLevel1Game1, newLevel1Game2, newLevel1Game3, new
 AllGamesData.prototype.getGameDataArray = function (levelNumber, gameNumber) {
     "use strict";
     var arrayName = "level" + levelNumber + "Game" + gameNumber;
-    // The break statements probably are not necessary
     switch (arrayName) {
         case ("level0Game1"): 
             return this.level0Game1;
-            break;
         case ("level0Game2"): 
             return this.level0Game2;
-            break;
+        
         case ("level0Game3"): 
             return this.level0Game3;
-            break;
+           
         case ("level0Game4"): 
             return this.level0Game4;
-            break;
+           
         case ("level0Game5"): 
             return this.level0Game5;
-            break;
+            
         case ("level0Game6"): 
             return this.level0Game6;
-            break;
+           
         case ("level1Game1"): 
             return this.level1Game1;
-            break;
+            
         case ("level1Game2"): 
             return this.level1Game2;
-            break;
+           
         case ("level1Game3"): 
             return this.level1Game3;
-            break;
+           
         case ("level1Game4"): 
             return this.level1Game4;
-            break;
+            
         case ("level2Game1"): 
             return this.level2Game1;
-            break;
+           
         case ("level2Game2"): 
             return this.level2Game2;
-            break;
+            
         case ("level2Game3"): 
             return this.level2Game3;
-            break;
+            
         case ("level2Game4"): 
             return this.leve2Game4;
-            break;
+           
         case ("level3Game1"): 
             return this.level3Game1;
-            break;
+            
         case ("level3Game2"): 
             return this.level3Game2;
-            break;
+          
         case ("level3Game3"): 
             return this.level3Game3;
-            break;
+            
         case ("level3Game4"): 
             return this.level3Game4;
-            break;
+            
         case ("level4Game1"): 
             return this.level4Game1;
-            break;
+            
         case ("level4Game2"): 
             return this.level4Game2;
-            break;
+            
         case ("level4Game3"): 
             return this.level4Game3;
-            break;
+           
         case ("level4Game4"): 
             return this.level4Game4;
-            break;        
+                 
         case ("level5Game1"): 
             return this.level5Game1;
-            break;
+            
         case ("level5Game2"): 
             return this.level5Game2;
-            break;
+            
         case ("level5Game3"): 
             return this.level5Game3;
-            break;
+            
         case ("level5Game4"): 
             return this.level5Game4;
-            break;        
+                  
         case ("level6Game1"): 
             return this.level6Game1;
-            break;
+            
         case ("level6Game2"): 
             return this.level6Game2;
-            break;
+           
         case ("level6Game3"): 
             return this.level6Game3;
-            break;
+            
         case ("level6Game4"): 
             return this.level6Game4;
-            break;                    
+                     
     }
 };
 
--- RocketReadingApp/src/mainController.js
+++ RocketReadingApp/src/mainController.js
@@ -83,9 +83,9 @@ var mainController = {
 		
     },
     
-    // loading player data from the local storage. 
+    
 	setPlayer: function (username) {
-    // This function is for 
+    // This function is for loading player data from the local storage 
         "use strict";
 		var playerData = storageController.getPlayer(username);
         console.log("mainController.setPlayer(): setPlayer() player's school - " + playerData.school);
@@ -319,7 +319,8 @@ var mainController = {
             level = rocketReadingModel.getCurrentGameData().getCurrentLevel(),
             levelNumber = level.getLevelNumber(),
             wordList = game.getWordList().slice(0),
-            levelGame = rocketReadingModel.getCurrentGameData().getCurrentLevelGame();
+            levelGame = rocketReadingModel.getCurrentGameData().getCurrentLevelGame(),
+            playerName = rocketReadingModel.getMyPlayer().getUserName();
         alert ("Game Finished! You completed Level "+ levelNumber + "Game " + gameNumber);
         // Stop the total game timer
         clearInterval(gameTimer);

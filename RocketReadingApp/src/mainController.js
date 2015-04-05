9fd1861f658f7cb715edb34914316f5b951b0555
michaeljh17 <mih279@student.cpit.ac.nz>
2015-04-06 11:35:18 +1200

    Replaying a game is working now

--- RocketReadingApp/src/mainController.js
+++ RocketReadingApp/src/mainController.js
@@ -722,8 +722,7 @@ var mainController = {
         // Determine which word the user will be tested on
         mainController.nextWord();
         // The event listener which led to this function being called will be removed
-        // An event listener is added to the start game button to do this
-        // myViewModelRR.removeEventListGameStart(); 
+        myViewModelRR.removeEventListGameStart(); 
     },
     
     startGameContinue: function () {
